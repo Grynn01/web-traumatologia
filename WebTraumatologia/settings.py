1360446a49a09cf2b42e58a400a5ff32071f879c
Grynn01 <nicoram.mt@gmail.com>
2018-02-23 20:13:21 -0300

    Fixed settings for pythoneverywhere

--- WebTraumatologia/settings.py
+++ WebTraumatologia/settings.py
@@ -119,8 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
