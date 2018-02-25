dbef199a799b1c75132a3182580850fea9dfb9dd
Grynn01 <nicoram.mt@gmail.com>
2018-02-24 23:13:12 -0300

    fixed buttons

--- WebTraumatologia/settings.py
+++ WebTraumatologia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1^j(97qm-q&w5_f!!+fq18wk@j0g!-8ii87e^dzjub-1pqs$^w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['grynn.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -73,11 +73,11 @@ WSGI_APPLICATION = 'WebTraumatologia.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+#'NAME': '/home/Grynn/web-traumatologia/WebTraumatologia.db'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/Grynn/web-traumatologia/WebTraumatologia.db',
+        'NAME': 'WebTraumatologia.db',
     }
 }
 
