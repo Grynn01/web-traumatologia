246dd21b9b02ffc73cb7bfe7e2695799064a74dc
Grynn01 <nicoram.mt@gmail.com>
2018-06-20 01:55:04 -0400

    commented admin imports

--- web/admin.py
+++ web/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import *
-from import_export.admin import ImportExportModelAdmin
-from import_export import fields, resources
-from import_export.widgets import ForeignKeyWidget
+#from import_export.admin import ImportExportModelAdmin
+#from import_export import fields, resources
+#from import_export.widgets import ForeignKeyWidget
 # Register your models here.
 
 
